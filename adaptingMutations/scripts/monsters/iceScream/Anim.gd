a4e14700ab0f53af3327b8f78f9fb23dd9a52df1
William <wbrines@pdx.edu>
2020-04-18 23:37:32 -0700

    added IceScream

---
+++ adaptingMutations/scripts/monsters/iceScream/Anim.gd
@@ -0,0 +1,75 @@
+extends AnimationPlayer
+
+var walking : int = 0;
+var attacking : bool = false;
+var _body : Sprite;
+var _hit : bool = false;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func init(body):
+	_body = body;
+	
+func sit():
+	_body.frame = 2
+	_body.offset = Vector2(10,-16);
+	
+func walk(dir : int = 0):
+	if(attacking == false):
+		if(walking != 0 && dir != walking) :
+			if(dir == 1):
+				play("Walk_down",-1,2);
+			if(dir == -1):
+				play("Walk_up",-1,2);
+		
+		if(walking == 0):
+			if(dir == 1):
+				play("Walk_down",-1,2);
+			if(dir == -1):
+				play("Walk_up",-1,2);
+		walking = dir;
+	
+func hit():
+	if(!_hit):
+		_hit = true;
+		play("Hit");
+		walking = false;
+		attacking = false;
+
+func attack():
+	if(attacking == false && _hit == false):
+		playback_speed = 1;
+		play("Attack");
+		attacking = true;
+		walking = false;
+		
+		
+func _on_Anim_animation_finished(anim_name):
+	match (anim_name):
+		"Walk_down": 
+			if(walking == 1):
+				play("Walk_down",-1,2);
+			if(walking == -1):
+				play("Walk_up",-1,2);
+			if(walking == 0):
+				_body.flip_h = false;
+				_body.frame = 0;
+				_body.rotation = 0.0;
+				
+		"Walk_up":
+			if(walking == 1):
+				play("Walk_down",-1,2);
+			if(walking == -1):
+				play("Walk_up",-1,2);
+			if(walking == 0):
+				_body.flip_h = false;
+				_body.frame = 1;
+				_body.rotation = 0.0;
+		"Hit":
+			_hit = false;
+		"Attack":
+			playback_speed = 1;
+			attacking = false;
+			walking = 0;
---
+++ adaptingMutations/scripts/monsters/iceScream/iceScream.gd
@@ -0,0 +1,76 @@
+extends Monster
+
+onready var animation = $Anim
+onready var _body = $Sprite
+const WALK_SPEED = 50;
+var faceing : int = 0;
+var following : bool = false;
+var attacking : bool = false;
+
+var _player = null;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation.init(_body);
+
+func _process(delta):
+	
+	var dir = Vector2(0,0);
+	if(attacking):
+		attack();
+	if(!animation.attacking):
+		if(animation._hit == false):
+			if(_player != null && following == true):
+				dir = _go_to_target(_player, delta);
+				
+			faceing = 1;
+			if(dir.y < 0): faceing = -1;
+				
+			if(dir != Vector2(0,0)):
+				if(faceing != 0):
+					animation.walk(faceing)
+			else:
+				animation.walk(0)
+				animation.sit();
+
+func _go_to_target(target : Node2D, delta) -> Vector2:
+	var dir : Vector2 = target.position - position;
+	dir = dir.normalized()
+	move_and_slide(Vector2(WALK_SPEED * cos(dir.angle()), WALK_SPEED  * sin(dir.angle())));
+
+	return dir;
+
+func attack():
+	animation.attack();
+
+func _on_FollowArea_body_entered(body):
+	if(body is Node2D):
+		if(body.name == "Player"):
+			_player = body;
+			following = true;
+	
+func _on_FollowArea_body_exited(body):
+	if(body is Node2D):
+		if(body.name == "Player"):
+			_player = null;
+			following = false;
+
+
+func _on_AtackArea_body_entered(body):
+	if(body is Node2D):
+		if(body.name == "Player"):
+			following = false;
+			attacking = true;
+
+
+func _on_AtackArea_body_exited(body):
+	if(body is Node2D):
+		if(body.name == "Player"):
+			following = true;
+			attacking = false;
+	
+func _take_damage(value : int):
+	animation.hit();
+	health -= value;
+	if(health < 0):
+		queue_free();
